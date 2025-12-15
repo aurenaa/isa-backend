fb682f3bb55c65867bf2e4ef1a1232bf883b1235
aurenaa <salapuranev@gmail.com>
2025-12-15 17:56:53 +0100

    Added modelmapper in main and packages

--- src/main/java/com/isa/backend/IsaBackendApplication.java
+++ src/main/java/com/isa/backend/IsaBackendApplication.java
@@ -1,10 +1,17 @@
 package com.isa.backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IsaBackendApplication {
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IsaBackendApplication.class, args);
